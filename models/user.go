c4843376010c3354dd6c78200e4cb14f7e4314f9
PwzXxm <cepheidfov@gmail.com>
2020-07-08 03:48:57 +1000

    add transaction support in user related functions

--- models/user.go
+++ models/user.go
@@ -14,45 +14,50 @@ const (
 	maxVerificationCodeTryCount = 100
 )
 
-// CheckUserExistence finds out whether an user is already existed or not
-// It takes a string represents the email
-func CheckUserExistence(u string) (bool, error) {
+func (t TX) CheckUserExistence(u string) (bool, error) {
 	var count int64
 	// TODO: not sure why the call to [Model] is required
-	err := db.Model(&User{}).Where(&User{Email: u}).Count(&count).Error
+	err := t.tx.Model(&User{}).Where(&User{Email: u}).Count(&count).Error
 	return count == 1, err
 }
 
 // CheckEmailVerificationExistence finds out whether an user's verification code is already existed or not
 // It takes a string represents the email
-func CheckEmailVerificationExistence(u string) (bool, error) {
+func (t TX) CheckEmailVerificationExistence(u string) (bool, error) {
 	var count int64
-	err := db.Model(&EmailVerification{}).Where(&EmailVerification{Email: u}).Where("expired_at >= ?", time.Now()).Count(&count).Error
+	err := t.tx.Model(&EmailVerification{}).Where(&EmailVerification{Email: u}).Where("expired_at >= ?", time.Now()).Count(&count).Error
 	return count == 1, err
 }
 
 // GetUserByEmail get the user's information
 // It takes an email and a struct to store the result
-// TODO: what should it return if the record not exists?
-func GetUserByEmail(u string) (*User, error) {
+func (t TX) GetUserByEmail(u string) (*User, error) {
 	var result User
-	err := db.Where(&User{Email: u}).First(&result).Error
+	err := t.tx.Where(&User{Email: u}).First(&result).Error
+
+	if IsErrNoDocument(err) {
+		return nil, nil
+	}
+
 	return &result, err
 }
 
 // GetEmailVerificationByEmail get the user's verification by email
 // It takes an email and a struct to store the result
-func GetEmailVerificationByEmail(u string) (*EmailVerification, error) {
+func (t TX) GetEmailVerificationByEmail(u string) (*EmailVerification, error) {
 	var result EmailVerification
-	err := db.Where(&EmailVerification{Email: u}).Where("expired_at >= ?", time.Now()).First(&result).Error
+	err := t.tx.Where(&EmailVerification{Email: u}).Where("expired_at >= ?", time.Now()).First(&result).Error
 	return &result, err
 }
 
 // GetUserByID get the user's information by his id,
 // it takes a id, and a result structure
-func GetUserByID(id int64) (*User, error) {
+func (t TX) GetUserByID(id int64) (*User, error) {
 	var result User
-	err := db.Where(&User{ID: id}).First(&result).Error
+	err := t.tx.Where(&User{ID: id}).First(&result).Error
+	if IsErrNoDocument(err) {
+		return nil, nil
+	}
 	return &result, err
 }
 
@@ -81,33 +86,45 @@ func generateVerificationCode() string {
 	return string(rst)
 }
 
-func CreateEmailVerification(u string) (string, error) {
+// CreateCreateEmailVerification create new verfication code associated with an email address
+func (t TX) CreateEmailVerification(u string) (string, error) {
+	userExist, err := t.CheckUserExistence(u)
+	if err != nil || userExist {
+		return "", err
+	}
+
+	// Email column is unique, create will fail if an entry with the same email adress exists
 	v := EmailVerification{
 		Email:            u,
 		VerificationCode: generateVerificationCode(),
 		RemainingCount:   maxVerificationCodeTryCount,
 		ExpiredAt:        time.Now().Add(time.Hour),
 	}
+
 	// TODO: create a new one or reuse the old one?
-	err := db.Create(&v).Error
+	err = t.tx.Create(&v).Error
+	if err != nil {
+		return "", err
+	}
+
 	return v.VerificationCode, err
 }
 
-func DeleteEmailVerification(e *EmailVerification) error {
-	return db.Delete(&e).Error
+func (t TX) DeleteEmailVerification(e *EmailVerification) error {
+	return t.tx.Delete(&e).Error
 }
 
-func UpdateEmailVerificationRemainingCount(e *EmailVerification) error {
-	return db.Select("remaining_count").Updates(e).Error
+func (t TX) UpdateEmailVerificationRemainingCount(e *EmailVerification) error {
+	return t.tx.Select("remaining_count").Updates(e).Error
 }
 
-func CreateUser(email string, pwdHash string) error {
+func (t TX) CreateUser(email string, pwdHash string) error {
 	user := User{
 		ID:       snowflakeNode.Generate().Int64(),
 		Email:    email,
 		Password: pwdHash,
 	}
-	err := db.Create(&user).Error
+	err := t.tx.Create(&user).Error
 	if err != nil {
 		return err
 	}
