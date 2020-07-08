58398476f450ab729e8fed2c6043c68467f53468
Patrick Weizhi Xu <cepheidfov@gmail.com>
2020-07-08 19:33:17 +1000

    add transaction support in user related functions (#42)
    * add Github Actions
    
    * Update build.yml
    
    * remove travis config file
    
    * update CI badge link
    
    * remove go111module in build action
    
    * add transaction support in user related functions
    
    * remove redundant checks
    
    * fix unchecked err

--- controllers/user.go
+++ controllers/user.go
@@ -28,14 +28,24 @@ const (
 
 // UserInfo returns users' information, including email
 func UserInfo(c *gin.Context) {
-	result, err := models.GetUserByID(c.MustGet("userId").(int64))
+	var userData *models.User
+
+	err := models.Transaction(func(tx models.TX) (err error) {
+		userData, err = tx.GetUserByID(c.MustGet("userId").(int64))
+		return
+	})
 	if err != nil {
 		InternalErrorResponse(c, err)
 		return
 	}
 
+	if userData == nil {
+		JSONResponse(c, CodeNotExist, "", nil)
+		return
+	}
+
 	JSONResponse(c, CodeOK, "", gin.H{
-		"email": result.Email,
+		"email": userData.Email,
 	})
 }
 
@@ -54,31 +64,24 @@ func UserLogin(c *gin.Context) {
 		return
 	}
 
-	// check if the user exists
-	userExist, err := models.CheckUserExistence(gEmail)
-	if err != nil {
-		InternalErrorResponse(c, err)
-		return
-	}
-	if !userExist {
-		JSONResponse(c, CodeNotExist, "sign up required", nil)
-		return
-	}
+	var userInfo *models.User
 
-	// check password
-	result, err := models.GetUserByEmail(gEmail)
+	err := models.Transaction(func(tx models.TX) (err error) {
+		userInfo, err = tx.GetUserByEmail(gEmail)
+		return
+	})
 	if err != nil {
 		InternalErrorResponse(c, err)
 		return
 	}
 
-	if !models.CheckPassword(gPassword, result.Password) {
+	if userInfo == nil || !models.CheckPassword(gPassword, userInfo.Password) {
 		JSONResponse(c, CodeAuthError, "incorrect email/password", nil)
 		return
 	}
 
 	JSONResponse(c, CodeOK, "", gin.H{
-		"token": utils.TokenGenerate(strconv.FormatInt(result.ID, 16)),
+		"token": utils.TokenGenerate(strconv.FormatInt(userInfo.ID, 16)),
 	})
 }
 
@@ -92,20 +95,30 @@ func UserGetSignUpVerification(c *gin.Context) {
 		return
 	}
 
-	// check existence of the user
-	userAlreadyExist, err := models.CheckUserExistence(gEmail)
+	var userAlreadyExist, userVerificationExist bool
+	var verificationCode string
+
+	err := models.Transaction(func(tx models.TX) (err error) {
+		userAlreadyExist, err = tx.CheckUserExistence(gEmail)
+		if userAlreadyExist || err != nil {
+			return
+		}
+
+		userVerificationExist, err = tx.CheckEmailVerificationExistence(gEmail)
+		if userVerificationExist || err != nil {
+			return
+		}
+
+		verificationCode, err = tx.CreateEmailVerification(gEmail)
+		return
+	})
 	if err != nil {
 		InternalErrorResponse(c, err)
-		return
-	}
-	if userAlreadyExist {
-		JSONResponse(c, CodeAlreadyExist, "", nil)
 		return
 	}
 
-	userVerificationExist, err := models.CheckEmailVerificationExistence(gEmail)
-	if err != nil {
-		InternalErrorResponse(c, err)
+	if userAlreadyExist {
+		JSONResponse(c, CodeAlreadyExist, "", nil)
 		return
 	}
 
@@ -114,25 +127,19 @@ func UserGetSignUpVerification(c *gin.Context) {
 		JSONResponse(c, CodeOK, "", gin.H{
 			"generated": false,
 		})
-	} else {
-		verificationCode, err := models.CreateEmailVerification(gEmail)
-
-		if err != nil {
-			InternalErrorResponse(c, err)
-			return
-		}
-
-		// we only send a verfication code once
-		// until it is invalid due to exceeding limits of trying
-		// or it expires
-
-		// TODO: handle the case where the email is failed to sent
-		utils.SendVerificationEmail(gEmail, verificationCode)
-
-		JSONResponse(c, CodeOK, "", gin.H{
-			"generated": true,
-		})
+		return
 	}
+
+	// we only send a verfication code once
+	// until it is invalid due to exceeding limits of trying
+	// or it expires
+
+	// TODO: handle the case where the email is failed to sent
+	utils.SendVerificationEmail(gEmail, verificationCode)
+
+	JSONResponse(c, CodeOK, "", gin.H{
+		"generated": true,
+	})
 }
 
 // UserCreateWithVerification checks verification code and create the user in the user database
@@ -156,78 +163,81 @@ func UserCreateWithVerification(c *gin.Context) {
 		return
 	}
 
-	// check if it is already in the Users table
-	userExist, err := models.CheckUserExistence(gEmail)
-	if err != nil {
-		InternalErrorResponse(c, err)
-		return
-	}
+	var userExist, userVerificationExist, exceedLimit, incorrectPwd bool
+	var emailVerifyInfo *models.EmailVerification
 
-	if userExist {
-		JSONResponse(c, CodeAlreadyExist, "", nil)
-		return
-	}
-
-	// check if the user exist in UserVerifications table
-	userVerificationExist, err := models.CheckEmailVerificationExistence(gEmail)
-	if err != nil {
-		InternalErrorResponse(c, err)
-		return
-	}
-
-	if !userVerificationExist {
-		JSONResponse(c, CodeOK, "", nil)
-		return
-	}
-
-	// check if the verification code is correct
-	result, err := models.GetEmailVerificationByEmail(gEmail)
-	if err != nil {
-		InternalErrorResponse(c, err)
-		return
-	}
-
-	// exceed the trying limit
-	if result.RemainingCount <= 0 {
-		err = models.DeleteEmailVerification(result)
-		if err != nil {
-			InternalErrorResponse(c, err)
+	err := models.Transaction(func(tx models.TX) (err error) {
+		// check if it is already in the Users table
+		userExist, err = tx.CheckUserExistence(gEmail)
+		if userExist || err != nil {
 			return
 		}
 
+		// check if the user exist in UserVerifications table
+		userVerificationExist, err = tx.CheckEmailVerificationExistence(gEmail)
+		if !userVerificationExist || err != nil {
+			return
+		}
+
+		// check if the verification code is correct
+		emailVerifyInfo, err = tx.GetEmailVerificationByEmail(gEmail)
+		if err != nil {
+			return
+		}
+
+		// gorm cannot update with zero value, unless using pointer in data struct
+		if emailVerifyInfo.RemainingCount <= 1 {
+			exceedLimit = true
+			err = tx.DeleteEmailVerification(emailVerifyInfo)
+			return
+		}
+		exceedLimit = false
+
+		if emailVerifyInfo.VerificationCode != gVerificationCode {
+			emailVerifyInfo.RemainingCount--
+			incorrectPwd = true
+			err = tx.UpdateEmailVerificationRemainingCount(emailVerifyInfo)
+			return
+		}
+		incorrectPwd = false
+
+		// insert to User table
+		hash, err := models.HashPassword(gPassword)
+		if err != nil {
+			return
+		}
+
+		err = tx.CreateUser(gEmail, hash)
+		if err != nil {
+			return
+		}
+
+		// delete used verification code
+		err = tx.DeleteEmailVerification(emailVerifyInfo)
+		return
+	})
+	if err != nil {
+		InternalErrorResponse(c, err)
+		return
+	}
+
+	// hide details to front-end
+	if userExist || !userVerificationExist {
+		JSONResponse(c, CodeAuthError, "", nil)
+		return
+	}
+
+	if exceedLimit {
 		JSONResponse(c, CodeAuthError, "exceed trying limit", gin.H{
 			"expired": true,
 		})
 		return
 	}
 
-	// incorrect verification code
-	if result.VerificationCode != gVerificationCode {
-		// reduce remaining trying count
-		result.RemainingCount -= 1
-		err = models.UpdateEmailVerificationRemainingCount(result)
-		if err != nil {
-			InternalErrorResponse(c, err)
-			return
-		}
-
+	if incorrectPwd {
 		JSONResponse(c, CodeAuthError, "incorrect verification code", gin.H{
 			"expired": false,
 		})
-		return
-	}
-
-	// insert to User table
-	hash, err := models.HashPassword(gPassword)
-	if err != nil {
-		InternalErrorResponse(c, err)
-		return
-	}
-
-	err = models.CreateUser(gEmail, hash)
-
-	if err != nil {
-		InternalErrorResponse(c, err)
 		return
 	}
 
