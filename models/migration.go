0056c82c9ca2440c564e54ab0d28876b1aae5ba9
PwzXxm <cepheidfov@gmail.com>
2020-07-16 23:25:11 +1000

    update migration

--- models/migration.go
+++ models/migration.go
@@ -21,24 +21,33 @@ func migrate(db *gorm.DB) error {
 
 		dbVersionInt, _ := strconv.Atoi(dbVersion.Value)
 
-		if dbVersionInt == 0 {
+		switch dbVersionInt {
+		case 0:
 			err = tx.AutoMigrate(&User{}, &EmailVerification{}, &NotificationMethod{}, &Sentry{}, &SentryImage{})
 			if err != nil {
 				return
 			}
-		} else {
-			if dbVersionInt == 1 {
-				err = db.Model(&Sentry{}).Where("notify_count = ?", -1).Update("notify_count", 0).Error
-				if err != nil {
-					return
-				}
-				err = tx.AutoMigrate(&User{}, &NotificationMethod{}, &Sentry{})
-				if err != nil {
-					return
-				}
+		case 1:
+			err = db.Model(&Sentry{}).Where("notify_count = ?", -1).Update("notify_count", 0).Error
+			if err != nil {
+				return
+			}
+			err = tx.AutoMigrate(&User{}, &NotificationMethod{}, &Sentry{})
+			if err != nil {
+				return
+			}
+		case 2:
+			err = tx.AutoMigrate(&EmailVerification{})
+			if err != nil {
+				return
+			}
+			err = db.Table("email_verifications").Migrator().DropColumn(&EmailVerification{}, "remaining_count")
+			if err != nil {
+				return
 			}
 		}
-		dbVersion.Value = "2"
+		dbVersion.Value = "3"
+
 		return tx.Save(&dbVersion).Error
 	})
 }
